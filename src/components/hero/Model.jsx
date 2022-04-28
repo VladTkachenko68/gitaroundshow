8a2392336549bba9d5aaeb2942c37d0dc87c6dd5
full stack developer <102779005+narakarax@users.noreply.github.com>
2022-04-28 03:29:48 -0700

    Update Model.jsx

--- src/components/hero/Model.jsx
+++ src/components/hero/Model.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import * as THREE from "three";
+import * as THREE from 'three'
+import { useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF, useAnimations } from "@react-three/drei";
-import { Scroll } from '@react-three/drei';
-import { ScrollControls, Sky, useScroll } from '@react-three/drei'
+import { useScroll } from '@react-three/drei'
+
 function Model() {
     const scroll = useScroll()  
   useFrame((state, delta) => {
     const offset = scroll.offset
     state.camera.position.set(Math.sin(offset * 4) * 10, Math.atan(offset * Math.PI * 2) * 4, Math.cos(offset * Math.PI) * -10)
-    state.camera.lookAt(0, 1, 0)
+    state.camera.lookAt(0, Math.atan(offset * Math.PI * 2) * 1, Math.cos(offset * Math.PI) * -4)
   })
 
   const { nodes, animations, materials } = useGLTF("/1.gltf");
@@ -33,12 +33,13 @@ function Model() {
             rotation={[-Math.PI / 2, 0, 0]}
             scale={100}
           >
-            <meshPhongMaterial color="#c0c0c0" />
+            <meshPhongMaterial color="#171616" emissive="#000000" specular="#292929" shininess={90} fog={true} reflectivity="1" refractionRatio={1} combine={THREE.MultiplyOperation}/>
           </skinnedMesh>
-        {/* </Scroll>
-      </ScrollControls> */}
     </group>
   );
 }
 
 export default Model;
+
+
+
